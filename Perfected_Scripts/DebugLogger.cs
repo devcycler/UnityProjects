0c7564636620ef684461c37ef4c9cbf80894c489
Xian <tmosier88@yahoo.com>
2022-06-18 14:27:08 -0400

    Static debug logger class

---
+++ Perfected_Scripts/DebugLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Utility
+{
+    //Credit: https://youtu.be/lRqR4YF8iQs
+    public static class DebugLogger
+    {
+        private static void DoLog(Action<string> LogFunction, string prefix, Object obj, object msg)
+        {
+#if UNITY_EDITOR
+            LogFunction($"{prefix}[<color=lightblue>{obj.name}</color>]: {msg}");
+#endif
+        }
+        public static void Log(this Object obj, object msg)
+        {
+            DoLog(Debug.Log, "[LOG]", obj, msg);
+        }
+
+        public static void LogError(this Object obj, object msg)
+        {
+            DoLog(Debug.LogError, "<color=red><!>[ERROR]</color>", obj, msg);
+        }        
+        public static void LogWarning(this Object obj, object msg)
+        {
+            DoLog(Debug.LogWarning, "<color=yellow>[WARNING]</color>", obj,  msg);
+        }
+
+        public static void LogSuccess(this Object obj, object msg)
+        {
+            DoLog(Debug.Log,"<color=green>[SUCCESS]</color>", obj, msg);
+        }
+    }
+}
